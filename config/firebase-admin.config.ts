2a8645f989397640b1fe14189d567adf7e998d91
Landon Yarrington <jly.dev.36963@gmail.com>
2020-10-01 17:32:16 -0600

    fix fb-admin config

--- config/firebase-admin.config.ts
+++ config/firebase-admin.config.ts
@@ -1,14 +1,11 @@
 const config = {
-  type: 'service_account',
-  project_id: process.env.FB_PROJECT_ID,
-  private_key_id: process.env.FB_PRIVATE_KEY_ID,
-  private_key: process.env.FB_PRIVATE_KEY,
-  client_email: process.env.FB_CLIENT_EMAIL,
-  client_id: process.env.FB_CLIENT_ID,
-  auth_uri: process.env.FB_AUTH_URI,
-  token_uri: process.env.FB_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_CERT,
-  client_x509_cert_url: process.env.FB_CLIENT_CERT,
+  projectId: process.env.FB_PROJECT_ID,
+  privateKeyId: process.env.FB_PRIVATE_KEY_ID,
+  privateKey: process.env.FB_PRIVATE_KEY,
+  clientEmail: process.env.FB_CLIENT_EMAIL,
+  clientId: process.env.FB_CLIENT_ID,
+  authUri: process.env.FB_AUTH_URI,
+  tokenUrl: process.env.FB_TOKEN_URI,
 };
 
 export default config;
